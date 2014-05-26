f77fe63e904c798562ab4b13f5dbbd9c65be4efd
Jace <lampsidewebdesign@gmail.com>
2014-05-26 13:08:18 -0500

    initial commit

--- handlers.py
+++ handlers.py
@@ -11,3 +11,19 @@ class MainHandler(webapp2.RequestHandler):
 	def get(self):
 		template = jinja_environment.get_template("index.html")
 		self.response.out.write(template.render())
+		
+class ContactHandler(webapp2.RequestHandler):
+	def get(self):
+		template = jinja_environment.get_template("contact.html")
+		self.response.out.write(template.render())
+		
+	def post(self):
+		fullname = cgi.escape(self.request.get("fullname"))
+		sender = cgi.escape(self.request.get("email"))
+		subject = cgi.escape(self.request.get("subject"))
+		message = cgi.escape(self.request.get("message"))
+		email = "Sender: " + sender + "\n" + "Full name: " + fullname + "\n" + "Message: " + "\n" + message
+		
+		_to = "CLIENT_EMAIL" # receiver
+		_from = "ADMIN_EMAIL" # application registrar
+		mail.send_mail(_from, _to, subject, email)
--- routes.py
+++ routes.py
@@ -3,4 +3,5 @@ from webapp2_extras.routes import RedirectRoute
 
 _routes = [
 	RedirectRoute('/', 'handlers.MainHandler', name='main', strict_slash=True),
+	RedirectRoute('/contact', 'handlers.ContactHandler', name='contact', strict_slash=True),
 ]
